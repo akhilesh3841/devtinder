fdd90a37fda9d56e4e70d9a059a56daa8a09f134
AKHILESH YADAV <122615318+akhilesh3841@users.noreply.github.com>
2024-10-08 00:42:03 +0530

    Update app.js

--- app.js
+++ app.js
@@ -11,3 +11,13 @@ app.listen(3000,()=>{
 })
 
 //how to push on github
+
+// git init ->mark karega saare files ko
+//.gitignore file bana kar->private aur bekar files ko nhi update krnege
+//git add . se saare files ko push honge
+//git commit -m "kuch bhi"
+//github pe jaake new resposutrt bnaahau
+//phir jo path milega terminak mein copy krna hai
+
+
+//how to push on github
